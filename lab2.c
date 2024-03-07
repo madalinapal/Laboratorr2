b20ab89fcaded40f46c5996bfa3d13ef92945b82
madalinapal <mdlpalade@gmail.com>
2024-03-07 11:30:03 +0200

    ModifcareElena

--- lab2.c
+++ lab2.c
@@ -3,20 +3,20 @@
 
 int function(int x){
 	x=x-2;
-	if(x>0)
-	 x=function(x);
+	/*if(x>0)
+	 x=function(x);*/
 	return x;
 }
 //err
 
 int main(){
 	int a=30, i=0;
-	
+
 	while (a>0){
 		a=function(a);
 		i++;
 	}
 	printf("%d", i);
-	return 0;	
+	return 0;
 }
 
